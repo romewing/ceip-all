95715592d3e40d400791736b0dae9e2ba387f66e
yi.zhang <yi.zhang@gh-ca.com>
2017-04-28 17:41:21 +0800

    导入

--- ceip-core/src/main/java/com/ghca/ceip/core/CEIPCoreApplication.java
+++ ceip-core/src/main/java/com/ghca/ceip/core/CEIPCoreApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
@@ -13,7 +12,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableJpaAuditing
-@EnableElasticsearchRepositories
 public class CEIPCoreApplication {
 
     public static void main(String[] args) {
